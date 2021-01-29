cd2949e215e25df2e18e9c7cf5f1fc982ab59b41
GregoryPWalton <75237506+GregoryPWalton@users.noreply.github.com>
2021-01-28 19:24:27 -0800

    gregory-walton1

--- index.js
+++ index.js
@@ -7,7 +7,7 @@
         + If a plane takes off, its `isFlying` property gets set to true.
         + If a plane lands, its `isFlying` property gets set to false.
 */
-
+     
 // EXAMPLE SOLUTION CODE:
 class Airplane {
     constructor(name) {
@@ -21,7 +21,7 @@ class Airplane {
       this.isFlying = false;
     }
   }
-  
+  6
   /*
   // 👇 COMPLETE YOUR WORK BELOW 👇
   // 👇 COMPLETE YOUR WORK BELOW 👇
